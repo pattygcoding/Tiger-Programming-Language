65aa1ac76cee00ccce44211132ece617925b8c03
phgoodw <pattygsocials@gmail.com>
2025-04-17 19:52:04 -0400

    gitignore modifications

--- rust/target/release/build/wasm-bindgen-shared-1502c08f601113a9/build_script_build-1502c08f601113a9.d
+++
@@ -1,5 +0,0 @@
-C:\Users\phgoodw\Documents\Tiger-Programming-Language\rust\target\release\build\wasm-bindgen-shared-1502c08f601113a9\build_script_build-1502c08f601113a9.exe: C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\build.rs
-
-C:\Users\phgoodw\Documents\Tiger-Programming-Language\rust\target\release\build\wasm-bindgen-shared-1502c08f601113a9\build_script_build-1502c08f601113a9.d: C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\build.rs
-
-C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-shared-0.2.100\build.rs:
--- rust/target/release/deps/wasm_bindgen_macro_support-e54253fdaf2acd3b.d
+++
@@ -1,8 +0,0 @@
-C:\Users\phgoodw\Documents\Tiger-Programming-Language\rust\target\release\deps\libwasm_bindgen_macro_support-e54253fdaf2acd3b.rmeta: C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\lib.rs C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\parser.rs
-
-C:\Users\phgoodw\Documents\Tiger-Programming-Language\rust\target\release\deps\libwasm_bindgen_macro_support-e54253fdaf2acd3b.rlib: C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\lib.rs C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\parser.rs
-
-C:\Users\phgoodw\Documents\Tiger-Programming-Language\rust\target\release\deps\wasm_bindgen_macro_support-e54253fdaf2acd3b.d: C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\lib.rs C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\parser.rs
-
-C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\lib.rs:
-C:\Users\phgoodw\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.100\src\parser.rs:
